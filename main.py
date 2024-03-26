ab23289ff0c0162959ef603f77770232928a9539
Cong Huynh <hnc203204@gmail.com>
2024-03-26 19:43:21 +0700

    can solve with n > 10

--- main.py
+++ main.py
@@ -5,24 +5,29 @@ import create_test
 from file_ope import *
 from func import *
 from tsp_algorithm import *
+import copy
 
 
-
-parent = []
-MAX = 4000000
+MAX = 50000
 dis = []
 INF = 10000000000
 
+class Generate:
 
-def generate_permutation(index: int, n : int, mask : int, state):
-    if index == n:
-        parent.append(state)
-        return
-    if len(parent) > MAX:
-        return
-    for it in range(0, n):
-        if (mask & (1 << it)) == 0:
-            generate_permutation(index + 1, n, mask | (1 << it), state + str(it))
+    def __init__(self):
+        self.parent = []
+    def generate_permutation(self, index: int, n : int, mask : int, state : Individual):
+        if index == n:
+            self.parent.append(state)
+            return
+        if len(self.parent) > MAX:
+            return
+        for it in range(0, n):
+            if (mask & (1 << it)) == 0:
+                new_state = copy.deepcopy(state)
+                new_state.add(it)
+                # print(new_state, state)
+                self.generate_permutation(index + 1, n, mask | (1 << it), new_state)
 
 def solve(**kwargs):
     n = 0
@@ -33,35 +38,40 @@ def solve(**kwargs):
         n, points = read_console()
     global dis, parent
     parent = []
-    generate_permutation(0, n, 0, "")
+    gener_per = Generate()
+    print(n)
+    gener_per.generate_permutation(0, n, 0, Individual())
 
     dis = create_distance_lst(n, points)
+    # for state in gener_per.parent:
+    #     print(state)
     def fit(state):
         total = 0
-        for index in range(0, len(state) - 1):
-            total += dis[int(state[index])][int(state[index + 1])]
+        try:
+            for index in range(0, len(state) - 1):
+                total += dis[state[index]][state[index + 1]]
+        except:
+            raise Exception(f"Loi o {index} {state}")
         return total
     try:
         algo = GA(n = n, points = points)
     except:
         raise Exception("Error at GA")
-
-    try:
-        algo1 = BruteForce(n = n, points= points)
-    except:
-        raise Exception("Error at BF")
-    print(algo.solve(population=parent[0: min(len(parent), 10000)], fit_func=fit), algo1.solve(parent, fit_func=fit))
-    return algo.solve(population=parent[0: min(len(parent), 100)], fit_func=fit)[1] == algo1.solve(parent, fit_func=fit)
-    # return 0
+    #
+    # try:
+    #     algo1 = BruteForce(n = n, points= points)
+    # except:
+    #     raise Exception("Error at BF")
+    print(algo.solve(population=gener_per.parent, fit_func=fit))
 
 
 if __name__ == "__main__":
     lst = [
-        1, 1, 1, 5, 5, 5, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10
+        10, 10, 10, 20, 20, 20, 30, 30, 30, 100, 100, 100
     ]
     # create_test.create_test(number_of_test=len(lst), lst = lst)
-    cnt = 0
-    for x in range(0, len(lst)):
-        cnt += solve(direction=f"test/test{x}.txt")
+    # cnt = 0
+    for x in range(3, len(lst)):
+        solve(direction=f"test/test{x}.txt")
 
-    print(cnt)
+    # print(cnt)
--- tsp_algorithm.py
+++ tsp_algorithm.py
@@ -17,6 +17,73 @@ def unique(list1):
     return unique_list
 
 
+class Individual:
+    def __init__(self, *args, **kwargs):
+        self.state = []
+        if "state" in kwargs.keys():
+            for ele in kwargs["state"]:
+                self.state.append(ele)
+        if "parent1" in kwargs.keys():
+            for ele in kwargs["parent1"]:
+                self.state.append(ele)
+            # self.state = kwargs["parent1"]
+        if "parent2" in kwargs.keys():
+            for ele in kwargs["parent2"]:
+                self.state.append(ele)
+        if "parent3" in kwargs.keys():
+            for ele in kwargs["parent3"]:
+                self.state.append(ele)
+
+    def __str__(self):
+        return str(self.state)
+
+    def add(self, numb : int):
+        self.state.append(numb)
+
+    def check(self):
+        check_arr = [0 for it in range(0, len(self.state))]
+        for x in self.state:
+            if check_arr[x] == 1:
+                return False
+            check_arr[x] = 1
+        return True
+
+    def fix(self):
+        check_arr = [0 for it in range(0, len(self.state))]
+        for x in self.state:
+            check_arr[x] += 1
+        unfill = []
+        for it in range(0, len(self.state)):
+            if check_arr[it] == 0:
+                unfill.append(it)
+
+        if len(unfill) > 0:
+            random.shuffle(unfill)
+            for index in range(0, len(self.state)):
+                if check_arr[self.state[index]] > 1 and len(unfill) > 0:
+                    check_arr[self.state[index]] -= 1
+                    self.state[index] = unfill[0]
+                    unfill.pop(0)
+
+        if not self.check():
+            raise Exception(f"Wrong {len(unfill)}, {self.state}")
+
+    def mutate(self, fit_func):#fix this mutate
+        index = random.randint(0, len(self.state) - 1)
+        index1 = random.randint(0, max(0, index - 1))
+        index2 = random.randint(min(index + 1, len(self.state) - 1), len(self.state) - 1)
+        new_child = self.state
+        if index1 >= 0 and self.state[index1] != self.state[index]:
+            new_child = swap_ele(self.state, index1, index)
+        if index2 <= len(self.state) - 1 and self.state[index2] != self.state[index]:
+            new_child1 = swap_ele(self.state, index2, index)
+            if fit_func(new_child) > fit_func(new_child1):
+                self.state = new_child1
+            else:
+                self.state = new_child
+
+
+
 class TSP:
     MAX = 200
     INF = 10000000000
@@ -27,12 +94,14 @@ class TSP:
     def get_min(self, population, fit):
         ans = TSP.INF
         for state in population:
-            ans = min(ans, fit(state))
+            ans = min(ans, fit(state.state))
         return ans
 
     @abstractmethod
     def solve(self, population, fit_func):
         pass
+
+
 
 class GA(TSP):
 
@@ -40,81 +109,45 @@ class GA(TSP):
         super().__init__(n=kwargs["n"], points = kwargs["points"])
 
     def random_choice(self, population):
-        size_random = int(len(population) / 2)
+        size_random = int(math.sqrt(len(population)))
         while True:
             index1 = random.randint(0, size_random)
             index2 = random.randint(0, size_random)
-            if population[index1] != population[index2]:
+            if population[index1].state != population[index2].state:
                 return population[index1], population[index2]
 
-    def check(self, child):
-        check_arr = [0 for it in range(0, len(child))]
-        for x in child:
-            if check_arr[int(x)] == 1:
-                return False
-            check_arr[int(x)] = 1
-        return True
-
-    def fix(self, child):
-        check_arr = [0 for it in range(0, len(child))]
-        for x in child:
-            check_arr[int(x)] += 1
-        unfill = []
-        for it in range(0, len(child)):
-            if check_arr[it] == 0:
-                unfill.append(it)
-
-        if len(unfill) > 0:
-            random.shuffle(unfill)
-            for index in range(0, len(child)):
-                if check_arr[int(child[index])] > 1 and len(unfill) > 0:
-                    check_arr[int(child[index])] -= 1
-                    new_child = child[0: index] + str(unfill[0])
-                    if index + 1 < len(child):
-                        child = new_child + child[index + 1:]
-                    unfill.pop(0)
-
-        if not self.check(child):
-            raise Exception(f"Wrong {len(unfill)}, {child}")
-        return child
-
     def reproduce(self, parent1, parent2):
-        index = random.randint(1, len(parent1) - 1)
+        index = random.randint(1, len(parent1.state) - 1)
         arr = [
-            self.fix(parent1[0: index] + parent2[index:]),
-            self.fix(parent2[0: index] + parent1[index:]),
-            self.fix(parent1[index:] + parent2[0:index]),
-            self.fix(parent2[index:] + parent1[0:index])
+            Individual(parent1=parent1.state[0: index], parent2=parent2.state[index:]),
+            Individual(parent1=parent2.state[0: index], parent2=parent1.state[index:]),
+            Individual(parent1=parent1.state[index:], parent2=parent2.state[0:index]),
+            Individual(parent1=parent2.state[index:], parent2=parent1.state[0:index])
         ]
-        index1 = random.randint(1, len(parent1) - 1)
-        if index1 < index:
-            index, index1 = index1, index
-        a1 = [parent1[0:index], parent2[index: index1], parent2[index1:]]
-        a2 = [parent2[0:index], parent1[index:index1], parent1[index1:]]
+        index1 = index
+        while index1 == index:
+            index1 = random.randint(1, len(parent1.state) - 1)
+            if index1 < index:
+                index, index1 = index1, index
+        a1 = [parent1.state[0:index], parent2.state[index: index1], parent2.state[index1:]]
+        a2 = [parent2.state[0:index], parent1.state[index:index1], parent1.state[index1:]]
+
         for x in range(0, 3):
+            # print(a1, a2, index, index1)
             random.shuffle(a1)
             random.shuffle(a2)
-            arr.append(self.fix(a1[0] + a1[1] + a1[2]))
-            arr.append(self.fix(a2[0] + a2[1] + a2[2]))
-
+            arr.append(Individual(parent1=a1[0], parent2=a1[1], parent3=a1[2]))
+            arr.append(Individual(parent1=a2[0], parent2=a2[1], parent3=a2[2]))
+        # for x in arr:
+        #     print(x.state)
+        for x in arr:
+            x.fix()
+            x.check()
         return arr
 
     def get_sorted_by_weigth(self, population, fit_func):
-        population.sort(key=lambda x: fit_func(x))
+        population.sort(key=lambda x: fit_func(x.state))
         return population
-
-    def mutate(self, child):#fix this mutate
-        index = random.randint(0, len(child) - 1)
-        for it in range(0, len(child)):
-            try:
-                if it != int(child[index]):
-                    for it1 in range(0, len(child)):
-                        if child[it1] == str(it):
-                            child = swap_string(child, it1, index)
-                            break
-            except:
-                raise Exception(f"Exception at: {child}")
-        return child
 
     def solve(self, population, fit_func):
         loop_count = 0
@@ -129,8 +162,9 @@ class GA(TSP):
             for i in range(0, len(population)):
                 parent1, parent2 = self.random_choice(sorted_population) #fix random choice
                 child_lst = self.reproduce(parent1, parent2)
+                # print(child_lst)
                 for child in child_lst:
-                    child = self.mutate(child) #fix mutate
+                    child.mutate(fit_func) #fix mutate
                     new_population.append(child)
                 if len(new_population) > 10000:
                     break
@@ -140,7 +174,7 @@ class GA(TSP):
             if delt > 0:
                 population = new_population
             else:
-                # print(delt, t)
+                print(delt, t)
                 if math.exp(-delt / t) > 0.0000001:
                     population = new_population
                 else:
@@ -149,8 +183,8 @@ class GA(TSP):
                 break
             loop_count += 1
         result_state = self.get_sorted_by_weigth(population, fit_func)[0]
-        result = fit_func(result_state)
-        return result_state, result
+        result = fit_func(result_state.state)
+        return result_state.state, result
 
 
 class BruteForce(TSP):
